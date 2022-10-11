805b82a0a65118fb6f6e3ac88a30f9a444d8f909
amieldelatorre <amieljames.delatorre@gmail.com>
2022-10-12 00:52:49 +1300

    DTO object for turning into json

---
+++ src/main/java/org/classes/ResultingClass.java
@@ -0,0 +1,24 @@
+package org.classes;
+
+import java.util.List;
+
+public class ResultingClass {
+    List<User> users;
+    List<Weight> weights;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Weight> getWeights() {
+        return weights;
+    }
+
+    public void setWeights(List<Weight> weights) {
+        this.weights = weights;
+    }
+}
